4ed047c45d9dcdf0ca6aa4985d4424b229e97cc0
Anthony Faraut <anthony.faraut@gmail.com>
2014-11-11 00:05:01 +0100

    JUnit attributs tests

--- projet/devoo/src/test/java/util/XMLVerificationTest.java
+++ projet/devoo/src/test/java/util/XMLVerificationTest.java
@@ -34,4 +34,46 @@ public class XMLVerificationTest {
 		File file = new File("src/main/resources/xml/plan20x20.xml"); 
         assertFalse(XMLVerification.checkLivraisonXML(file));
 	}  
+	
+	@Test 
+	public void testcheckLivraisonXML_NOK_Attribut_Entrepot_adresse() {
+		// Bad XML file
+		File file = new File("src/test/resources/xml/livraison10x10-1.xml"); 
+        assertFalse(XMLVerification.checkLivraisonXML(file));
+	}  
+	
+	@Test 
+	public void testcheckLivraisonXML_NOK_Attribut_Plage_heureDebut() {
+		// Bad XML file
+		File file = new File("src/test/resources/xml/livraison10x10-2.xml"); 
+        assertFalse(XMLVerification.checkLivraisonXML(file));
+	}  
+	
+	@Test 
+	public void testcheckLivraisonXML_NOK_Attribut_Plage_heureFin() {
+		// Bad XML file
+		File file = new File("src/test/resources/xml/livraison10x10-3.xml"); 
+        assertFalse(XMLVerification.checkLivraisonXML(file));
+	}  
+	
+	@Test 
+	public void testcheckLivraisonXML_NOK_Attribut_Livraison_id() {
+		// Bad XML file
+		File file = new File("src/test/resources/xml/livraison20x20-1.xml"); 
+        assertFalse(XMLVerification.checkLivraisonXML(file));
+	}  
+	
+	@Test 
+	public void testcheckLivraisonXML_NOK_Attribut_Livraison_client() {
+		// Bad XML file
+		File file = new File("src/test/resources/xml/livraison20x20-2.xml"); 
+        assertFalse(XMLVerification.checkLivraisonXML(file));
+	} 
+	
+	@Test 
+	public void testcheckLivraisonXML_NOK_Attribut_Livraison_adresse() {
+		// Bad XML file
+		File file = new File("src/test/resources/xml/livraison20x20-3.xml"); 
+        assertFalse(XMLVerification.checkLivraisonXML(file));
+	} 
 }
