4734be907a62d618496c8acf6d155cd24f457123
ldanilescu <ludmila.danilescu@insa-lyon.fr>
2014-11-11 18:33:02 +0100

    Version Finale Calcul du plus court chemin Dijkstra

--- projet/devoo/src/main/java/util/Vertex.java
+++ projet/devoo/src/main/java/util/Vertex.java
@@ -1,18 +1,49 @@
 package util;
 import model.data.Point;
+import model.data.Troncon;
+import java.util.ArrayList;
 
 public class Vertex implements Comparable<Vertex> {
 
-    public Point point;
-    public double minDistance = Double.POSITIVE_INFINITY;
-    public Vertex precedent;
+    private Point point;
+    private ArrayList<Troncon> adjacencies;
+    private double minTemps = Double.POSITIVE_INFINITY;
+    private Vertex precedent;
+    
     public Vertex(Point argPoint) 
     { 
-    	point = argPoint; 
+    	this.point = argPoint;
+    	this.adjacencies = this.point.getTronconsSortants();
     }
+    
     public int compareTo(Vertex autre)
     {
-        return Double.compare(minDistance, autre.minDistance);
+        return Double.compare(getMinTemps(), autre.getMinTemps());
     }
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public Vertex getPrecedent() {
+		return this.precedent;
+	}
+
+	public void setPrecedent(Vertex precedent) {
+		this.precedent = precedent;
+	}
+
+	public ArrayList<Troncon> getAdjacencies() {
+		return adjacencies;
+	}
+
+	public double getMinTemps() {
+		return minTemps;
+	}
+
+	public void setMinTemps(double minTemps) {
+		this.minTemps = minTemps;
+	}
+
 
 }
