ca22d89af1b6669f1303880e63eb6dd275c9a510
MariaETEGAN <maria.etegan@insa-lyon.fr>
2014-11-13 22:56:11 +0100

    delete useless

--- projet/devoo/src/test/java/devoo/LivraisonManagerTest.java
+++ projet/devoo/src/test/java/devoo/LivraisonManagerTest.java
@@ -80,37 +80,7 @@ public class LivraisonManagerTest {
 		  System.out.println("Lalal");
 		
 	}
-	@Test
-	public void testSuppresionLivraison() {
-		Controller controller = new Controller();
-		PlanManager planManager = new PlanManager(controller);
-		LivraisonManager livraisonManager = new LivraisonManager(planManager, controller);
-		Noeud n1 = new Noeud(1,5,7);
-		Noeud n2 = new Noeud(2,5,9);
-		Noeud n3 = new Noeud(3,10,9);
-		Troncon t12 = new Troncon("rue12", 10, 52, n1,n2);
-		Troncon t23 = new Troncon("rue23", 5, 40, n2,n2);
-		Troncon t31 = new Troncon("rue31", 7, 60, n3,n1);
-		Livraison l1 = new Livraison(1,20,n1);
-		Livraison l2 = new Livraison(2,20,n2);
-		Livraison l3 = new Livraison(3,20,n3);
-		n1.setLivraison(l1);
-		n2.setLivraison(l2);
-		n3.setLivraison(l3);
-		n1.setIsLivraison(true);
-		n2.setIsLivraison(true);
-		n3.setIsLivraison(true);
-		Chemin c12 = new Chemin(n1,n2,t12); 
-		Chemin c23 = new Chemin(n2,n3,t23); 
-		Chemin c31 = new Chemin(n3,n1,t31); 
-		List<Chemin> mesChemins = new ArrayList<Chemin>();
-		Itineraire it = new Itineraire(mesChemins);
-		livraisonManager.setItineraire(it);
-		
-		
-		livraisonManager.supprimerLivraison(n2);
-
-	}	
+	
 
 
 }
