e0bca4d2012d8f7f63bfe969d1af5a0940d4e213
ldanilescu <ludmila.danilescu@insa-lyon.fr>
2014-11-14 04:21:00 +0100

    Javadoc

--- projet/devoo/src/test/java/devoo/LivraisonManagerTest.java
+++ projet/devoo/src/test/java/devoo/LivraisonManagerTest.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import model.data.Chemin;
 import model.data.Itineraire;
-import model.data.Livraison;
 import model.data.Noeud;
-import model.data.Troncon;
 import model.manager.LivraisonManager;
 import model.manager.PlanManager;
 
@@ -20,32 +18,30 @@ import controller.Controller;
 
 public class LivraisonManagerTest {
 	
+	/**
+	 * 
+	 */
 	@Test
 	public void testTransfHeureMinANDFirstBefSecond() {
 		float testFloat = 7800.0f;
-		Controller controller = new Controller();
-		PlanManager planManager = new PlanManager(controller);
-		LivraisonManager livraisonManager = new LivraisonManager(planManager, controller);
 		String testString = CalculesHoraires.transformeEnHeureMin(testFloat);
-		//System.out.println(testString);
 		Assert.assertEquals(testString, "02:10");	
 
 		testFloat = 7860.0f;
 		String testString2 = CalculesHoraires.transformeEnHeureMin(testFloat);
-		//System.out.println(testString2);
 		Assert.assertEquals(testString2, "02:11");	
 		
 		Boolean res = CalculesHoraires.firstBeforeSecond(testString2, testString);
-		//System.out.println(res);
+		System.out.println(res);
 	}
 	
+	 /**
+	  * 
+	  */
 	@Test
 	public void testSommeHeures() {
 		String heureA = "12:10";
 		String heureB = "0:55";
-		Controller controller = new Controller();
-		PlanManager planManager = new PlanManager(controller);
-		LivraisonManager livraisonManager = new LivraisonManager(planManager, controller);
 		String testSomme = CalculesHoraires.sommeHeures(heureA, heureB);
 		
 		System.out.println(testSomme);
@@ -59,15 +55,10 @@ public class LivraisonManagerTest {
 		System.out.println(testSomme);
 		Assert.assertEquals(testSomme, "15:12");	
 	}
-	
-	@Test
-	public void testAddLivraison() {
-		Controller controller = new Controller();
-		PlanManager planManager = new PlanManager(controller);
-		LivraisonManager livraisonManager = new LivraisonManager(planManager, controller);
-
-	}	
-	
+		
+	/**
+	 * 
+	 */
 	@Test
 	public void testExporterFeuilleRoute() {
 
@@ -87,13 +78,10 @@ public class LivraisonManagerTest {
 		Noeud c = v.get(4).getPoint();		
 				
 		chemin1 = planManager.calculerPlusCourtChemin(a,b);   //entre le premier et le dernier noeuds de la liste
-		System.out.println("Le plus court chemin est" + chemin1.toString());
 		
 		chemin2 = planManager.calculerPlusCourtChemin(b,c);   //entre le premier et le dernier noeuds de la liste
-		System.out.println("Le plus court chemin est" + chemin2.toString());
 		
 		chemin3 = planManager.calculerPlusCourtChemin(c,a);   //entre le premier et le dernier noeuds de la liste
-		System.out.println("Le plus court chemin est" + chemin3.toString());
 		
 		listChemins.add(chemin1);
 		listChemins.add(chemin2);
